91e8d9412df0a5f869ec08911956fa0726ad0117
sha-melon <62735464+sha-melon@users.noreply.github.com>
2021-09-15 08:58:09 +0000

    init

---
+++ src/apollo/client.js
@@ -0,0 +1,42 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+
+export const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://info.hpdex.org/subgraphs/name/davekaj/uniswap',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const healthClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://info.hpdex.org/graphql',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const v1Client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const stakingClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://info.hpdex.org/subgraphs/name/davekaj/uniswap',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const blockClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://info.hpdex.org/subgraphs/name/blocklytics/ethereum-blocks',
+  }),
+  cache: new InMemoryCache(),
+})
